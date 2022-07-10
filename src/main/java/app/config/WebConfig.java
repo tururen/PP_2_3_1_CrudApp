dbfa6256de4f95ca77bccdaefdd61796032700fa
tur <bonus90@mail.ru>
2022-07-10 19:58:51 +0300

    added Autowired

--- src/main/java/app/config/WebConfig.java
+++ src/main/java/app/config/WebConfig.java
@@ -1,5 +1,6 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +18,8 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 public class WebConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+
+    @Autowired
     public WebConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
