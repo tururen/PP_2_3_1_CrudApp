7fac177e5bea33261a6bf606a3e322054d97f849
tur <bonus90@mail.ru>
2022-07-10 20:02:00 +0300

    Override toString, hashCode, equals

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -1,6 +1,7 @@
 package app.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name ="users")
@@ -58,5 +59,28 @@ public class User {
 
     public void setAge(byte age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
